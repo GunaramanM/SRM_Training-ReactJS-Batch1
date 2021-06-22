306dc5222ac6cbd08600c6b4c6f14fbe662c11dc
GunaramanM <86225498+GunaramanM@users.noreply.github.com>
2021-06-22 10:42:54 +0530

    Update Day1.js

--- Day1/Day1.js
+++ Day1/Day1.js
@@ -1,43 +1,23 @@
-// 2
-console.log(" 2) To check if string input is numeric")
 
-var val = 2345;
-g = isNaN(val);
-console.log(g);
-if (g == false) {
-    console.log("it is a number");
-}
-var val = "2345";
-g = isNaN(val);
-console.log(g);
-if (g == false) {
-    console.log("it is a number");
-}
-var val = "This is a string";
-g = isNaN(val);
-console.log(val + ' ' + g);
-if (g == false) {
-    console.log("it is a number");
-}
-
-// 3
-console.log("3) convert a number to string and vice versa");
+console.log("convert a number to string & string to number");
 console.log("string to number using parseInt");
 let string1 = "this is string";
 let strTonum = parseInt(string1);
 let type = typeof (strTonum);
 console.log("type of" + strTonum + " is " + type);
-
 console.log("number to string");
 let number = 1500;
 number += '';
 let numbertype = typeof (number);
-console.log("Type of numbertype::" + numbertype);
+console.log("Type of numbertype::" + numbertype); 
 
-// 4
-console.log("4) == and ===");
-var value4 = "787";
-var value5 = 787;
+
+
+
+
+console.log("== and ===");
+var value4 = "1030";
+var value5 = 1030;
 
 if (value4 == value5) {
     console.log("True since it  compares oly the values");
@@ -53,60 +33,28 @@ else {
 }
 
 
-// 5 
-console.log("5) var and let");
 
-//using var
-for (var i = 0; i < 3; i++) {
-    console.log("var inside " + i);
-}
-console.log("outside loop still accessed" + i);
 
-//using let
-for (let j = 0; j < 3; j++) {
-    console.log("inside loop" + j);
 
-}
-// console.log("outside loop"+j);
-
-// 6
-console.log("6) storing date in variable");
+console.log("storing date in variable");
 const d = new Date();
 console.log(d);
 
-// 7
-console.log("7) storing past date");
+
+
+console.log("storing past date");
 const today = new Date()
 const yesterday = new Date(today)
-
 yesterday.setDate(yesterday.getDate() - 1)
-
 today.toDateString()
 yesterday.toDateString()
-console.log("Yesterday's date::" + yesterday);
+console.log("Yesterday's date::" + yesterday); 
 
 
-// 8
 
-console.log("8) Prefix er for even elements /dr for odd elements");
-const username = ["Gordan Ramsay", "Harikrish", "Liya", "Shalini", "Nikhil"];
-const alter = username.map((uname, idx) => {
-    if (idx % 2 == 0) {
-        return 'Er.' + uname;
-    }
-    else {
-        return 'Dr.' + uname;
-    }
-}
-);
-for (let i = 0; i < alter.length; i++) {
-    console.log(alter[i]);
-}
 
-// 9
-
-console.log("9) append san for names starting with Dr");
-const usernames = ["Gordan Ramsay", "Harikrish", "Liya", "Shalini", "Nikhil"];
+console.log("append san for names starting with Dr");
+const usernames = ["Guna", "Gokul", "Subeesh", "Mahesh", "Amal"];
 const alters = usernames.map((unames, idx) => {
     if (idx % 2 == 0) {
         return 'Er.' + unames;
@@ -129,10 +77,11 @@ for (let i = 0; i < alters.length; i++) {
 
 }
 
-// 10
-console.log("10) Extract prime numbers");
 
-const newArray = [23, 57, 93, 12, 5, 37, 84, 22, 10, 11];
+
+console.log("prime numbers");
+
+const newArray = [21, 47, 37, 12, 10, 37, 84, 22, 5, 11];
 const PrimeArray = newArray.filter(num => {
     for (let i = 2; i < num; i++) {
         if (num % i === 0) return false;
@@ -142,3 +91,23 @@ const PrimeArray = newArray.filter(num => {
 console.log("array::" + newArray);
 console.log("Prime numbers in array::" + PrimeArray);
 
+
+
+
+
+let Student = [];
+
+Student[0] = { id: "a001", name: "guna", marks: 45, city: "Chennai" };
+Student[1] = { id: "a002", name: "amal", marks: 30, city: "Madurai" };
+Student[2] = { id: "a003", name: "varsha", marks: 28, city: "Karur" };
+Student[3] = { id: "a004", name: "mahesh", marks: 65, city: "Chennai" };
+Student[4] = { id: "a005", name: "ashwin", marks: 75, city: "Chennai" };
+Student[5] = { id: "a006", name: "Ramya", marks: 45, city: "Banglore" };
+console.log("add 20 marks to all stuednts");
+
+let addmarks = Student.map(addmarks => {
+    let mar = addmarks.marks + 20;
+
+    console.log(`NAME: ${addmarks.name} Stuednt id: ${addmarks.id} Mark: ${mar} City: ${addmarks.city}`);
+
+}); 
